41f4c78ecc32013bf6e0b8edd21ebf924240f162
amarajit <jeetu_a1@rediffmail.com>
2018-03-16 15:00:10 +0530

    Bootstrap issue fixed

--- src/app/services/workout.service.ts
+++ src/app/services/workout.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Workout } from '../model/workout';
+import { ServiceUrlProviderService } from './service-url-provider.service';
 
 @Injectable()
 export class WorkoutService {
 
-  private workoutUrl = "http://localhost:8090/workouts/add";
-  constructor(private http: HttpClient) { }
+  private workoutUrl : string;
+  constructor(private http: HttpClient,
+    private urlProv: ServiceUrlProviderService) {
+      this.workoutUrl = urlProv.getCompleteServiceUrl("workouts/add");
+     }
   
   createWorkout(workout: any): Observable<any>{
     return this.http.post<any>(this.workoutUrl, workout);
--- src/app/services/workoutList.service.ts
+++ src/app/services/workoutList.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ServiceUrlProviderService } from './service-url-provider.service';
+
 
 @Injectable()
 export class WorkoutListService {
 
-  private getWorkoutListUrl = "http://localhost:8090/workouts/user/";
+  private getWorkoutListUrl: string;
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private urlProv: ServiceUrlProviderService
+  ) {
+    this.getWorkoutListUrl = urlProv.getCompleteServiceUrl("workouts/user/");
+  }
 
-  getWorkoutList(userName : string): Observable<any>{
-       return this.http.get<any>(this.getWorkoutListUrl+userName);
+  getWorkoutList(userName: string): Observable<any> {
+    return this.http.get<any>(this.getWorkoutListUrl + userName);
   }
 
 }
